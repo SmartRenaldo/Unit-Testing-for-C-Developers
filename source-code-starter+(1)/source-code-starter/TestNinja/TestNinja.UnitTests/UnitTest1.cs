0cf4454f19dcf8ec8a0a86260b0f4f7a33f6ff4b
RENALDO LUMIENT <renaldo@lumient.com.au>
2022-11-10 22:39:44 +1030

    add other two tests for CanBeCancelledBy method in Reservation class

--- source-code-starter+(1)/source-code-starter/TestNinja/TestNinja.UnitTests/UnitTest1.cs
+++ source-code-starter+(1)/source-code-starter/TestNinja/TestNinja.UnitTests/UnitTest1.cs
@@ -19,5 +19,35 @@ namespace TestNinja.UnitTests
             // Assert
             Assert.IsTrue(result);
         }
+
+        [TestMethod]
+        public void CanBeCancelledBy_MadeByUser_ReturnsTrue()
+        {
+            // Arrange
+            var reservation = new Reservation();
+
+            // Act
+            var s = new User() { IsAdmin = false };
+            reservation.MadeBy = s;
+            var result = reservation.CanBeCancelledBy(s);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_UserIsNotAdminAndNotMadeByUser_ReturnsFalse()
+        {
+            // Arrange
+            var reservation = new Reservation();
+
+            // Act
+            var s = new User() { IsAdmin = false };
+            reservation.MadeBy = s;
+            var result = reservation.CanBeCancelledBy(new User() { IsAdmin = false });
+
+            // Assert
+            Assert.IsFalse(result);
+        }
     }
 }
