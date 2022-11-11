a5be27bdec47e7f34ebb6c72389abe5ff799dd96
RENALDO LUMIENT <renaldo@lumient.com.au>
2022-11-11 14:32:29 +1030

    add test case of ReturnTheSumOfArguments

---
+++ source-code-starter+(1)/source-code-starter/TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+using Math = TestNinja.Fundamentals.Math;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class MathTests
+    {
+        [Test]
+        public void Add_WhenCalled_ReturnTheSumOfArguments()
+        {
+            var math = new Math();
+            var res = math.Add(1, 2);
+            Assert.That(res, Is.EqualTo(3));
+        }
+    }
+}
