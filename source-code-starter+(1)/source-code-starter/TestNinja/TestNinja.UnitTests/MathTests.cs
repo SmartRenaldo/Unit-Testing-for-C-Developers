9d274da1cf8912184692b7c46110fcc90fff08a9
RENALDO LUMIENT <renaldo@lumient.com.au>
2022-11-12 19:36:34 +1030

    extract common code by SetUp method

--- source-code-starter+(1)/source-code-starter/TestNinja/TestNinja.UnitTests/MathTests.cs
+++ source-code-starter+(1)/source-code-starter/TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -12,35 +12,39 @@ namespace TestNinja.UnitTests
     [TestFixture]
     public class MathTests
     {
+        Math _math;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Math();
+        }
+
         [Test]
         public void Add_WhenCalled_ReturnTheSumOfArguments()
         {
-            var math = new Math();
-            var res = math.Add(1, 2);
+            var res = _math.Add(1, 2);
             Assert.That(res, Is.EqualTo(3));
         }
 
         [Test]
         public void Max_FirstArgumentIsGreater_ReturnTheFirstArgument()
         {
-            var math = new Math();
-            var res = math.Max(2, 1);
+            var res = _math.Max(2, 1);
             Assert.That(res, Is.EqualTo(2));
         }
 
         [Test]
         public void Max_SecondArgumentIsGreater_ReturnTheSecondArgument()
         {
-            var math = new Math();
-            var res = math.Max(0, 1);
+            var res = _math.Max(0, 1);
             Assert.That(res, Is.EqualTo(1));
         }
 
         [Test]
         public void Max_ArgumentsAreEqual_ReturnTheSameArgument()
         {
-            var math = new Math();
-            var res = math.Max(1, 1);
+            var res = _math.Max(1, 1);
             Assert.That(res, Is.EqualTo(1));
         }
     }
