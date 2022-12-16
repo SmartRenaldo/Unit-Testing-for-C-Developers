da28fb0259108f4e2450a67c8e234e8ee11fe90a
RENALDO LUMIENT <renaldo@lumient.com.au>
2022-12-16 23:00:27 +1030

    add tests for EmployeeController

---
+++ source-code-starter+(1)/source-code-starter/TestNinja/TestNinja.UnitTests/EmployeeControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class EmployeeControllerTests
+    {
+        [Test]
+        public void DeleteEmployee_WhenCalled_DeleteTheEmployeeFromDb()
+        {
+            var employeeService = new Mock<IEmployeeService>();
+            var controller = new EmployeeController(employeeService.Object);
+
+            controller.DeleteEmployee(1);
+
+            employeeService.Verify(s => s.DeleteEmployee(1));
+        }
+
+    }
+}
