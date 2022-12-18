843606619c8994ac0bac7f474ee4f1f45f2ac2aa
RENALDO LUMIENT <renaldo@lumient.com.au>
2022-12-18 15:35:58 +1030

    add a test for BookingHelper when booking starts and finishes before an existing booking

---
+++ source-code-starter+(1)/source-code-starter/TestNinja/TestNinja.UnitTests/BookingHelper_OverlappingBookingsExistTests.cs
@@ -0,0 +1,65 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TestNinja.Mocking;
+
+[TestFixture]
+public class BookingHelper_OverlappingBookingsExistTests
+{
+    private Booking _existingBooking;
+    private Mock<IBookingRepository> _repository;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _existingBooking = new Booking
+        {
+            Id = 2,
+            ArrivalDate = ArriveOn(2022, 12, 15),
+            DepartureDate = DepartOn(2022, 12, 20),
+            Reference = "a"
+        };
+
+        _repository = new Mock<IBookingRepository>();
+        _repository.Setup(r => r.GetActiveBookings(1)).Returns(new List<Booking>
+            {
+                _existingBooking
+
+            }.AsQueryable());
+    }
+
+    [Test]
+    public void BookingStartsAndFinishesBeforeAnExistingBooking_ReturnEmptyString()
+    {
+        var result = BookingHelper.OverlappingBookingsExist(new Booking
+        {
+            Id = 1,
+            ArrivalDate = Before(_existingBooking.ArrivalDate, days: 2),
+            DepartureDate = Before(_existingBooking.ArrivalDate)
+        }, _repository.Object);
+
+        Assert.That(result, Is.Empty);
+    }
+
+    private DateTime Before(DateTime dateTime, int days = 1)
+    {
+        return dateTime.AddDays(-days);
+    }
+
+    private DateTime After(DateTime dateTime, int days = 1)
+    {
+        return dateTime.AddDays(days);
+    }
+
+    private DateTime ArriveOn(int year, int month, int day)
+    {
+        return new DateTime(year, month, day, 14, 0, 0);
+    }
+
+    private DateTime DepartOn(int year, int month, int day)
+    {
+        return new DateTime(year, month, day, 10, 0, 0);
+    }
+}
